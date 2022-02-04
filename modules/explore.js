678f09b5008293b21f19ab308b8a7db1ebe1b912
Abhijit <abhijitshendagescoe@gmail.com>
2022-02-04 23:42:22 +0530

    File size and ..

--- modules/explore.js
+++ modules/explore.js
@@ -2,6 +2,14 @@ const fs = require("fs");
 const path_mod = require("path");
 
 
+// getting file size
+bytesToSize = (bytes) => {
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes == 0) return '0 Byte';
+  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
 itsDirectory = (resource, res) => {
   
   fs.readdir(resource, function (err, files) {
@@ -17,8 +25,17 @@ itsDirectory = (resource, res) => {
 
     files.forEach((file) => {
       fileInfos.push({
-        name: file,
-        url: baseUrl + file,
+        type  : fs.lstatSync(
+                decodeURIComponent(
+                  path_mod.join(resource, file)
+                )).isDirectory()
+                ?"directory":"file",
+        name  : file,
+        url   : baseUrl + file,
+        size  : bytesToSize(
+                fs.statSync(decodeURIComponent(
+                path_mod.join(resource, file)
+                )).size)
       });
     });
 
@@ -56,7 +73,7 @@ const Filter = (req, res, path) => {
     else
     {
       console.error("Resource not found")
-      res.status(404).send(
+      res.status(500).send(
         {
           message: "Requested resource not found!"
         }
@@ -65,7 +82,7 @@ const Filter = (req, res, path) => {
 
   } catch (err) {
     console.error("Soemthing went wrong..", err)
-    res.status(404).send(
+    res.status(500).send(
       {
         message: "Internal Error"
       }
